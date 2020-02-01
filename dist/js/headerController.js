0c71af5cb7bf444cddee6983e6ae9f507476347f
Kyle McCrosson <mccrosson.k@husky.neu.edu>
2020-02-01 17:50:09 -0500

    begin redesign of page with angular routing

--- dist/js/headerController.js
+++ dist/js/headerController.js
@@ -1,14 +1,26 @@
-let app = angular.module('siteApp', []);
+let app = angular.module('siteApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: 'dist/html/home-page.html'
+    })
+    .when('/music', {
+      templateUrl: 'dist/html/music-page.html'
+    })
+    .when('/sound_design', {
+      templateUrl: 'dist/html/sound-design-page.html'
+    })
+});
 
 app.controller('headerController', function($scope) {
 
   $scope.sections = {
-    'Home': '#0',
-    'Music': '#1',
-    'Sound Design': '#2',
+    'Home': "#/!",
+    'Music': "#!music",
+    'Sound Design': "#!sound_design",
     'Data Viz': 'https://northeastern-ds-4200-f19.github.io/project-team-9-pedestrians',
-    'Contact': '#3'
-  }
-
+    'Contact': "#!contact"
+  };
 
 });
