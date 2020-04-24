0d409e5fa8a6d01542ac5d796522a94a892e93cc
Kyle McCrosson <mccrosson.k@husky.neu.edu>
2020-04-24 18:09:49 -0400

    Updates

--- src/components/layout.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import layoutStyle from './layout.module.css'
-
-export default ({children}) => <div className={layoutStyle.content}>{children}</div>
--- src/components/leftFloatText.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import LeftTextStyle from './leftFloatText.module.css'
-
-export default ({children}) => <div className={LeftTextStyle.Text}>{children}</div>
